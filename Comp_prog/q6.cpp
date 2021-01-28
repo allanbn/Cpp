1c707e0d7249f91e72026a100c1e82ea29a3866c
leftabn <leftabn@DESKTOP-9HG2DJS.localdomain>
2021-01-28 09:57:57 -0300

    Adjusted to the correct solution after a while without practicing

--- Comp_prog/q6.cpp
+++ Comp_prog/q6.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 
-#define lli long long int
-
-using namespace std;
-
-lli sol(lli x, lli y, lli z, lli c) {
-    printf("%lld\n", z);
-    if (z == 1 || z == -1) return 1;
-    sol(x, y, z+x, c+1);
-    sol(x, y, z+y, c+1);
-    sol(x, y, z-x, c+1);
-    sol(x, y, z-y, c+1); 
-    return 0;
-}
-
-
 int main() {
-    lli D, E, H, c;
-    c = 0;
-    H = 0;
-    cin >> D >> E;
-    if (sol(D, E, H, c)) printf("%lld\n", c);
+
+    long int D, E;
+
+    scanf("%ld %ld", &D, &E);
+
+    int n = D;
+    int c = 1;
+
+    while ((n > 1) || (n < -1)) {
+        if (n >= 1) n -= E;
+        else n += D;
+        c++;
+    }
+    
+    if (!n) printf("IMPOSSIVEL\n");
+    else printf("%ld\n", c);
+
     return 0;
 }
