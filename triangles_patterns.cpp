d190b67229d50210714c067532dbdae9a0f3e20b
leftabn <allanbispoxd@gmail.com>
2021-02-22 05:03:52 -0300

    Cpp script to print some triangles patterns

--- triangles_patterns.cpp
+++ triangles_patterns.cpp
@@ -1,33 +1,109 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
+#define LINES 3
 
 using namespace std;
-/**
- *                            *  
- *                           ***
- *                          *****
- *                           ***
- *                            *
-*/
-int main()
+
+void halfPyramid(int lines)
 {
-  int i, j, k, n;
-  // parte de cima
-  for (i = 1; i <= (5/2)+1; i++)
+  /**
+   * The pattern is:
+   *     *
+   *     **
+   *     ***
+  */
+  int i, j;
+  for (i = 1; i <= lines; i++)
   {
-    for (j = 1; j <= 5-i; j++)
-    {
-      cout << " ";
-    }
-    for (k = 1; k <= 2*i-1; k++)
+    for (j = 1; j <= i; j++)
     {
       cout << "*";
     }
-    cout << endl;
+    cout << "\n";
   }
-  // parte de baixo
-  for (i = (5/2); i >= 1; i--)
+  return;
+}
+
+void halfPyramidToLeft(int lines)
+{
+  /**
+   * The pattern is:
+   *      *
+   *     **
+   *    ***
+  */
+  int i, j;
+  for (i = 1; i <= lines; i++)
   {
-    for (j = 5-i; j >= 1; j--)
+    for (j = 1; j <= lines-i; j++)
+    {
+      cout << " ";
+    }
+    for (j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return;
+}
+
+void halfUpsideDownPyramid(int lines)
+{
+  /**
+   * The pattern is:
+   *     ***
+   *     **
+   *     *
+  */
+  int i, j;
+  for (i = 1; i <= lines; i++)
+  {
+    for (j = i; j <= lines; j++)
+    {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return;
+}
+
+void halfUpsideDownPyramidToLeft(int lines)
+{
+  /**
+   * The pattern is:
+   *     ***
+   *      **
+   *       *
+  */
+  int i, j;
+  for (i = 1; i <= lines; i++)
+  {
+    for (j = 1; j < i; j++)
+    {
+      cout << " ";
+    }
+    for (j = i; j <= lines; j++)
+    {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return;
+}
+
+void fullUpsideDownPyramid(int lines)
+{
+  /**
+   * The pattern is:
+   *     *****
+   *      ***
+   *       *
+  */
+  int i, j, k;
+  for (i = lines; i >= 1; i--)
+  {
+    for (j = lines-i; j >= 1; j--)
     {
       cout << " ";
     }
@@ -35,7 +111,89 @@ int main()
     {
       cout << "*";
     }
-    cout << endl;
+    cout << "\n";
   }
+  return;
+}
+
+void fullPyramid(int lines)
+{
+  /**
+   * The pattern is:
+   *      *
+   *     ***
+   *    *****
+  */
+  int i, j, k;
+  for (i = 1; i <= lines; i++)
+  {
+    for (j = 1; j <= lines-i; j++)
+    {
+      cout << " ";
+    }
+    for (k = 1; k <= 2*i-1; k++)
+    {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return;
+}
+
+void diamondPattern(int lines)
+{
+/**
+ * The pattern is:
+ *      *  
+ *     ***
+ *    *****
+ *     ***
+ *      *
+*/
+  int i, j, k, n;
+  // Upside print
+  for (i = 1; i <= (lines/2)+1; i++)
+  {
+    for (j = 1; j <= lines-i; j++)
+    {
+      cout << " ";
+    }
+    for (k = 1; k <= 2*i-1; k++)
+    {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  // Downside print
+  for (i = (lines/2); i >= 1; i--)
+  {
+    for (j = lines-i; j >= 1; j--)
+    {
+      cout << " ";
+    }
+    for (k = i*2-1; k >= 1; k--)
+    {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return;
+} 
+
+int main()
+{
+  halfPyramid(LINES);
+  cout << "\n";
+  halfUpsideDownPyramid(LINES);
+  cout << "\n";
+  halfPyramidToLeft(LINES);
+  cout << "\n";
+  halfUpsideDownPyramidToLeft(LINES);
+  cout << "\n";
+  diamondPattern(LINES);
+  cout << "\n";
+  fullPyramid(LINES);
+  cout << "\n";
+  fullUpsideDownPyramid(LINES);
   return 0;
 }
